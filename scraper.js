39436a1580106fd89c4a60c203841ef18e12d94e
Zach Latta <zach@zachlatta.com>
2025-02-23 02:18:32 +0000

    Word's simplest scraper

---
+++ scraper.js
@@ -0,0 +1,34 @@
+import { parse } from "node-html-parser"
+
+async function indexUrl(url, depth=0) {
+  if (depth < 0) {
+    return
+  }
+
+  const response = await fetch(url)
+  const html = await response.text()
+
+  const root = parse(html)
+  
+  let text = root.textContent
+
+  let links = root.querySelectorAll('a').map(link => {
+    const href = link.getAttribute('href')
+    if (!href) return null
+    try {
+      // resolve relative urls
+      return new URL(href, url).href
+    } catch {
+      return null
+    }
+  }).filter(Boolean)
+  
+  console.log(text) // todo clean up text
+  console.log(links.toString())
+
+  for (let link of links) {
+    await indexUrl(link, depth - 1)
+  }
+}
+
+await indexUrl('https://zachlatta.com', 2)
